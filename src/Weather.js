678794d2c07bdf059c096a3ce7c410d976139d46
Shaina <97140374+borahae13@users.noreply.github.com>
2022-10-31 15:08:27 -0400

    Took out weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -63,7 +62,6 @@ export default function Weather(props) {
               </div>
             </form>
             <WeatherInfo data={weatherData} />
-            <WeatherForecast coordinates={weatherData.coordinates} size={30} />
           </div>
         </div>
       </div>
